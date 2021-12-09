f2be972dccd4ff75fe7cab9d6c26f101914f6a4a
Natalia Tasci <nt@ilbers.de>
2021-12-09 11:25:19 +0100

    Add handling NoSuchElementException, lesson 3.6

--- test_items.py
+++ test_items.py
@@ -2,12 +2,18 @@ import pytest
 import time
 
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 class TestItem():
     
     def test_item_page_should_contain_add_button(self, browser):
+    
         browser.get(link)
-        time.sleep(5)
-        assert browser.find_element_by_css_selector(".btn-add-to-basket")
+        time.sleep(1)
+        ele = None
+        try:
+            ele = browser.find_element_by_css_selector(".btn-add-to-bsket")
+        except NoSuchElementException:
+            assert ele != None, "Add to basket button is missing"
